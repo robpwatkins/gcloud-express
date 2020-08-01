a81c170c04f7049ddff58790ae1d027b7ffb667f
Rob W <robpwatkins@gmail.com>
2020-08-01 17:04:04 -0500

    hmmmmm

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yooooooo <code>src/App.js</code> and save to reload.
+          Yooooo <code>src/App.js</code> and save to reload.
         </p>
         <button onClick={async () => {
           const response = await fetch('/pups');
